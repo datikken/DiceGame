34ec9d2dbdf033e6917782dd20b631c97c8beb7a
tikken <tikken23@gmail.com>
2019-03-28 01:50:29 +0300

    winner

--- app.js
+++ app.js
@@ -21,19 +21,33 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice
         document.querySelector('#current-' + activePlayer).textContent = roundScore
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore = 0
-
-        document.getElementById('current-0').textContent = '0'
-        document.getElementById('current-1').textContent = '0'
-
-        // document.querySelector('.player-0-panel').classList.remove('active')
-        // document.querySelector('.player-1-panel').classList.add('active')
-
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
-        document.querySelector('.dice').style.display = 'none'
+        nextPlayer()
     }
-
 })
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add current score to global score
+    scores[activePlayer] += roundScore
+    //update the ui
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+    //check if player won the game
+    if(scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner'
+    } else {
+        //Next player
+        nextPlayer()   
+    }
+})
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+}
